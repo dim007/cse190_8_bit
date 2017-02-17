8d27700987411468d0f5115694737c959e01f7f7
dim007 <dim007@ucsd.edu>
2017-02-17 11:16:58 -0800

    Nothing

---
+++ main.asm
@@ -0,0 +1,145 @@
+	
+
+
+                org 33000
+	di
+
+title	ld hl, 16384            ;clear screen
+        ld de, 16385
+        ld bc, 6143           
+        ld (hl), 0
+        ldir                    ;increment hl de, decrement bc
+        ld b, 192               ;192 lines to draw
+        ld ix, 0                ;start at 0,0
+        call getPixelAddr
+                                ;move address to de reg
+        ld d, h
+        ld e, l           
+        ld hl, TITLE           ;title reference
+
+lineloop 
+        ld a, b
+        ld bc, 32               ;copy graphic data to display file
+        ldir
+        ld b,a
+        inc ixh                 ;next row byte
+        ld ixl, 0
+        push hl
+        call getPixelAddr
+        ld d, h
+        ld e, l  
+        pop hl                
+        djnz lineloop
+        
+       
+        ld de, 22528            ;set attribute color
+        ld bc, 768
+        ldir
+        ld a, 0
+        call 8859               ;set border color
+        ei 
+
+main	
+	
+	;normal main
+        ld a, (playPos_y)       ;load init y player position
+	ld ixh,a
+        ld a, (playPos_x)       ; x position
+	ld ixl,a
+	call getPixelAddr
+
+	ld de,ash1              ; ref graphic data
+        ld b, 16                ; draw 16 rows
+
+        call DrawAsh
+        ld iy,0                 ;32 columns to draw
+
+DrawPlatforms
+        ld ixl, 0               ; x position
+
+ChangedX
+        ld ixh, 184             ; y position
+	call getPixelAddr
+        ld de,platform          ; ref graphic data
+        ld b, 8                 ; draw 8 rows
+
+DrawNextCell
+
+        ld a,(de)               ;load first byte
+	ld (hl),a               ;write to screen mem
+        inc de                  ;get next byte
+        inc ixh                 ;get next row byte address
+        call getPixelAddr
+	djnz DrawNextCell
+
+        inc iy                  ;next column
+        ld a, iyl
+        cp 32
+        inc ixl 
+        jp nz,ChangedX
+
+
+MainLoop
+        ;load player position
+	ld a,(playPos_y)
+	ld (OLDy),a
+	ld a,(playPos_x)
+	ld (OLDx),a
+
+	call Gravity	
+	;check for jump movement
+        ld bc,32766             ;keyboard b,n,m,shift,space
+        in a,(c)
+        rra
+        call nc,Jump
+	
+	;check for L/R movement
+	ld bc, 65022            ;keyboard asdfg ports
+        in a, (c)               ;what keys were pressed
+        rra                     ;was "a" pressed?
+        push af      
+        call nc,MoveLeft
+        pop af
+        rra		        ;rotate right, skip "s" for now
+	rra		        ;rotate right for "d" key
+	push af
+	call nc,MoveRight
+	pop af
+	call clearMe
+	call drawMe
+	;store player position
+	ld a,ixh
+	ld (playPos_y),a
+	ld a,ixl
+	ld (playPos_x),a
+	xor a	; clear a
+	ld (ISMOVING),a	;stop movement animation
+	jp MainLoop
+        
+gameover
+
+	ret; 
+
+INCLUDE movement.asm
+INCLUDE render.asm     
+INCLUDE ash.asm
+INCLUDE title.asm
+
+platform
+
+        DEFB	255,255,129,129,255,129,129,129
+	DEFB	 56
+
+press_start
+	DEFB	22,16,10,"Press Space"	;At,16,12
+	defb 	13			;new line
+eostr equ $
+menu1_string
+	DEFB	22,16,12,"Start Game"	
+	DEFB	13
+eostr2 equ $
+menu2_string
+	DEFB	22,19,12,"Controls"
+	DEFB	13
+eostr3 equ $
+
---
+++ render.asm
@@ -0,0 +1,114 @@
+
+getPixelAddr            ;Source:http://www.animatez.co.uk/computers/zx-spectrum/screen-memory-layout/
+	
+        LD A,ixh                ; Calculate Y2,Y1,Y0
+        AND %00000111           ; Mask out unwanted bits
+        OR %01000000            ; Set base address of screen
+        LD H,A                  ; Store in H
+        LD A,ixh                ; Calculate Y7,Y6
+        RRA                     ; Shift to position
+        RRA
+        RRA
+        AND %00011000           ; Mask out unwanted bits
+        OR H                    ; OR with Y2,Y1,Y0
+        LD H,A                  ; Store in H
+        LD A,ixh                ; Calculate Y5,Y4,Y3
+        RLA                     ; Shift to position
+        RLA
+        AND %11100000           ;  Mask out unwanted bits
+        LD L,A                  ; Store in L
+        LD A,ixl                ; Calculate X4,X3,X2,X1,X0
+        RRA                     ; Shift into position
+        RRA
+        RRA
+        AND %00011111           ; Mask out unwanted bits
+        OR L                    ; OR with Y5,Y4,Y3
+        LD L,A                  ; Store in L
+        RET
+
+;SCREEN 1 START
+DrawAsh
+        di                      ;disable interrupts
+       
+       
+        ld (SCRNADDR), hl
+        ld iy,BKGRNDBUFF
+        ld b,16                 ;16 rows to save
+
+STOREBACKGROUND
+        
+        call getPixelAddr
+        ld a,(hl)               ;get first byte
+        ld (iy), a              ;store byte in buffer
+        inc iyl                 ;addr of next buf address
+        inc l                   ;next byte of data
+
+        ld a,(hl)
+        ld (iy),a
+        inc iyl
+        inc ixh
+        djnz STOREBACKGROUND
+
+       
+        ld hl,(SCRNADDR)
+        ld a, (playPos_y)       ;load init y player position
+	ld ixh,a
+        ld a, (playPos_x)       ; x position
+	ld ixl,a
+        ld b, 16
+        ld de, ash1
+
+DrawSprite
+        
+        ld a,(de)               ;load first byte
+	ld (hl),a               ;write to screen mem
+        inc de                  ;get next byte 
+        inc hl                  ;get adjecent 8x8 cell
+        ld a,(de)               ;load adj cell
+        ld (hl),a
+        inc de                  ;get next byte
+        inc ixh                 ;get next row byte address
+        call getPixelAddr
+	djnz DrawSprite
+        ei
+        ret        
+        
+
+wait
+        ld bc,$1fff
+	dec bc
+	ld a,b
+	or c
+	jr nz, wait
+        ret
+
+ClearSprite
+        
+        ld a,(iy)
+        ld (hl),a
+        inc iyl                 ;get adjecent 8x8 cell
+        inc l
+        ld a,(iy)
+        ld (hl),a
+        inc iyl                ;get next byte
+        inc ixh                 ;get next row byte address
+        call getPixelAddr
+	djnz ClearSprite
+	ld a,(playPos_y)
+        ld ixh,a
+        ret
+
+oldPlayPos_x:
+        DEFB 0
+oldPlayPos_y:
+        DEFB 0
+playPos_x:
+	DEFB 50
+playPos_y:
+	DEFB 167
+curPos_x:
+        DEFB 39
+curPos_y:
+        DEFB 123
+isJump:
+	DEFB 0
